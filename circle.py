2229adc1a8327ee1f1c6c912e70c47f68954bfc8
coding-yost <77023930+coding-yost@users.noreply.github.com>
2021-02-16 21:06:46 -0800

    Added more books

---
+++ circle.py
@@ -0,0 +1,13 @@
+class circle:
+
+    
+    PI = 3.14
+    radius = 0
+
+    def __init__(self, radius):
+
+        self.radius = radius
+
+    def diameter():
+
+        return radius * 2
---
+++ main.py
@@ -0,0 +1,8 @@
+import circle
+class main:
+
+    if '__name__' == '__main__':
+        O = circle(9)
+        print(o.diameter())
+
+    main()
